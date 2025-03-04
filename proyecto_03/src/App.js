35269e5e0684ebaa8d3e35e441c74874cbe8a4d8
Emir Huaroc <emirmho3@gmail.com>
2025-03-04 12:32:32 -0500

    3er proyecto

---
+++ proyecto_03/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Cualquier></Cualquier>
+    </div>
+  );
+}
+
+
+const Cualquier =()=>{
+  return(
+    <div>
+      <h1>Hola que tal</h1>
+      <br></br>
+      <p>sasdodas <strong>pollo</strong> co papapas</p>
+    </div>
+  )
+}
+
+export default App;
